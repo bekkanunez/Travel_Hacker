72b54e0552ec026736b588f4e3497fbfe1a35eef
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-10-11 01:59:26 -0500

    updated my code

---
+++ index.js
@@ -0,0 +1,9 @@
+//variables for html elements with references
+var fetchButton = document.getElementById("fetch-button");
+
+/// show tickets/events
+function ticketMaster(e) {
+  import("./components/Ticket_Master/script.js");
+}
+
+fetchButton.addEventListener("click", ticketMaster);
